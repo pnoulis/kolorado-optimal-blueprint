35a77938f210f9f903c994eb2a56cabc2b3d2fc7
pavlos noulis <pavlos.noulis@gmail.com>
2024-11-02 15:53:10 +0200

    shapes interface

---
+++ src/api/shapes.js
@@ -0,0 +1,6 @@
+function create_shapes() {}
+function read_shapes() {}
+function update_shapes() {}
+function delete_shapes() {}
+
+export { create_shapes, read_shapes, update_shapes, delete_shapes };
