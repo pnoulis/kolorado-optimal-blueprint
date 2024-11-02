8c694bcb4d58d0763227999bfcfe90b2273e995c
pavlos noulis <pavlos.noulis@gmail.com>
2024-11-02 08:25:03 +0200

    Replace powerset dependency with own implementation

--- src/combinatorics.js
+++ src/combinatorics.js
@@ -1,7 +1,6 @@
 function factorial(number) {
   return number > 1 ? number * factorial(--number) : 1;
 }
-
 export function count_combinations(objects, arrangement) {
   // C(n,r) -> n! / n! * (n-r)!
   return (
@@ -9,25 +8,35 @@ export function count_combinations(objects, arrangement) {
     (factorial(arrangement) * factorial(objects - arrangement))
   );
 }
-
 export function count_permutations(objects, arrangement) {
   // P(n,r) -> n! / (n-r)!
   return factorial(objects) / factorial(objects - arrangement);
 }
+export function count_cardinality(set) {
+  return set.length;
+}
+export function count_powerset_permutations(cardinality_set) {
+  return 2 ** cardinality_set;
+}
+export function powerset(set, onPermutation) {
+  const cardinality_set = count_cardinality(set);
+  const cardinality_ps = count_powerset_permutations(cardinality_set);
+  let permutation_index = 0;
+  let permutation = [];
 
-function permute(objects) {
-  let result = [];
-  const _permute = (arr, m = []) => {
-    if (arr.length === 0) {
-      result.push(m);
-    } else {
-      for (let i = 0; i < arr.length; i++) {
-        let curr = arr.slice();
-        let next = curr.splice(i, 1);
-        _permute(curr.slice(), m.concat(next));
-      }
+  while (permutation_index <= cardinality_ps) {
+    for (let i = 0; i < cardinality_set; i++) {
+      if (permutation_index & (2 ** i))
+        permutation.push({
+          set_index: i,
+          set_value: set[i],
+        });
     }
-  };
-  _permute(objects);
-  return result;
+    onPermutation({
+      permutation_index: permutation_index,
+      permutation: [...permutation],
+    });
+    permutation = [];
+    permutation_index++;
+  }
 }
