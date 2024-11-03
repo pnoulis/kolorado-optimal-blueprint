4d2459514e2b0acd3baaf4ad0f4d75577049b06f
pavlos noulis <pavlos.noulis@gmail.com>
2024-11-04 00:56:35 +0200

    utilize workspaces for sharing code

---
+++ packages/common/index.js
@@ -0,0 +1,6 @@
+export * from './optimal-blueprint.js';
+export * from './Kerror.js';
+export * from './combinatorics.js';
+export * from './tsc.js';
+export * from './blueprints.js';
+export * from './shapes.js';
