08c21a32d9b3c3fef6e918aad2b848356f819fab
pavlos noulis <pavlos.noulis@gmail.com>
2024-11-19 16:43:35 +0200

    more errors

--- packages/common/Kerror.js
+++ packages/common/Kerror.js
@@ -11,57 +11,100 @@ class Kerror extends Error {
     super(
       typeof runtime === "string"
         ? runtime
-        : runtime?.message || defaults.message || cause?.message,
+        : runtime?.message ||
+            defaults.message ||
+            defaults.statusText ||
+            cause?.message ||
+            cause?.statusText,
       { cause: runtime instanceof Error ? runtime : cause },
     );
     this.name = defaults.name;
-    this.code = runtime?.code || defaults.code || cause?.code;
+    this.errno =
+      runtime?.errno ||
+      runtime?.status ||
+      defaults.errno ||
+      defaults.status ||
+      cause?.errno ||
+      cause?.status;
+    this.status = runtime?.status || defaults.status || cause?.status;
+    this.statusText =
+      runtime?.statusText || defaults.statusText || cause?.statusText;
     Error.captureStackTrace(this, this.constructor);
     const lines = this.stack.split("\n");
     this.stack = [lines[0], ...lines.slice(2)].join("\n");
     return this;
   }
-  static ERR_INVALID_ARGUMENTS(runtime, cause) {
+
+  /* Client Errors (400) */
+  static ERR_BAD_REQUEST(runtime, cause) {
+    // Request body/headers invalid
     return new Kerror(
       {
         name: "ERR_INVALID_ARGUMENTS",
         message: "Invalid Arguments",
-        code: 400,
-      },
-      { runtime, cause },
-    );
-  }
-  static ERR_INVALID_RESPONSE(runtime, cause) {
-    return new Kerror(
-      {
-        name: "ERR_INVALID_RESPONSE",
-        message: "Invalid API Response",
-        code: 500,
+        status: 400,
+        statusText: "Bad Request",
       },
       { runtime, cause },
     );
   }
   static ERR_UNAUTHENTICATED(runtime, cause) {
+    // API Consumer Unidentified
     return new Kerror(
-      { name: "ERR_UNAUTHENTICATED", message: "Unauthenticated", code: 401 },
+      {
+        name: "ERR_UNAUTHENTICATED",
+        message: "Unauthenticated",
+        status: 401,
+        statusText: "Unauthorized",
+      },
       { runtime, cause },
     );
   }
   static ERR_UNAUTHORIZED(runtime, cause) {
+    // API Consumer missing permissions
     return new Kerror(
-      { name: "ERR_UNAUTHORIZED", message: "Unauthorized", code: 403 },
+      {
+        name: "ERR_UNAUTHORIZED",
+        message: "Unauthorized",
+        status: 403,
+        statusText: "Forbidden",
+      },
       { runtime, cause },
     );
   }
   static ERR_NOT_FOUND(runtime, cause) {
+    // Resource Missing
     return new Kerror(
-      { name: "ERR_NOT_FOUND", message: "Not Found", code: 404 },
+      {
+        name: "ERR_NOT_FOUND",
+        message: "Not Found",
+        status: 404,
+        statusText: "Not Found",
+      },
       { runtime, cause },
     );
   }
+  static ERR_API(runtime, cause) {
+    // An unfulfilled condition in the API
+    return new Kerror(
+      {
+        name: "ERR_API",
+        message: "Unable to service the request",
+        status: 422,
+        statusText: "Unprocessable Entity",
+      },
+      { runtime, cause },
+    );
+  }
+  /* Server Errors (500) */
   static ERR_SERVER(runtime, cause) {
     return new Kerror(
-      { name: "ERR_SERVER", message: "Internal Server Error", code: 500 },
+      {
+        name: "ERR_SERVER",
+        message: "Internal Server Error",
+        status: 500,
+        statusText: "Internal Server Error",
+      },
       { runtime, cause },
     );
   }
