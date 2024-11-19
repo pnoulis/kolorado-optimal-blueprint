f7390133cb79615a9770737199070f09c284ee5d
pavlos noulis <pavlos.noulis@gmail.com>
2024-11-19 21:03:43 +0200

    no longer use for states, shape deletions are not allowed if in use

--- packages/server/src/sqlite.js
+++ packages/server/src/sqlite.js
@@ -1,25 +1,15 @@
-import { debug } from "common";
+import { Kerror } from "common";
 import sqlite from "better-sqlite3";
-
-/**
- * Shape State
- * @typedef {number} ShapeState - active(0), deleted(1)
- */
 
 /**
  * Shape
  * @typedef {Object} Shape
  * @property {string} id
  * @property {string} name
- * @property {ShapeState} state
  */
 
 /**
- * Blueprint State
- * @typedef {number} BlueprintState - active(0), deleted(1), invalid(2)
- */
-
-/**
+ * BlueprintShape
  * @typedef {Object} BlueprintShape
  * @extends Shape
  * @property {number} count
@@ -30,25 +20,92 @@ import sqlite from "better-sqlite3";
  * @typedef {Object} Blueprint
  * @property {string} id
  * @property {string} name
- * @property {BlueprintState} state
  * @property {BlueprintShape[]} shapes
+ */
+
+/**
+ * SqliteResponse
+ * @typedef {Object} SqliteResponse
+ * @property {number} changes
+ * @property {number} lastInsertRowid
  */
 
 const _db = new sqlite(process.env.DB_URL);
 const db = {
   /**
-   * @returns {Shape}
+   * Create shape
+   * @param {Shape} shape
+   * @returns {SqliteResponse}
    */
-  createShapes: (() => {
-    const one = _db.prepare(
-      "INSERT INTO shape (name, state) VALUES (@name, @state)",
-    );
-    return function (shapes) {
-      return one.run(shapes);
+  createShape: (() => {
+    const one = _db.prepare("INSERT INTO shape (name) VALUES (@name)");
+    return function (shape) {
+      return one.run(shape);
     };
   })(),
   /**
-   * @returns {Shape[]|Shape}
+   * Delete shape
+   * @param {string} shape
+   * @param {string} [key=id]
+   * @returns {SqliteResponse}
+   */
+  deleteShape: (() => {
+    const oneId = _db.prepare("DELETE FROM shape WHERE id=?");
+    const oneName = _db.prepare("DELETE FROM shape WHERE name=?");
+    return function (shape, key) {
+      let sqliteResponse;
+      key ||= "id";
+      sqliteResponse = key === "id" ? oneId.run(shape) : oneName.run(shape);
+    };
+  })(),
+
+  /**
+   * Create blueprint
+   * @param {Blueprint} blueprint
+   * @returns {SqliteResponse}
+   */
+  createBlueprint: (() => {
+    const one = _db.prepare("INSERT INTO blueprint (name) VALUES (@name)");
+    return function (blueprint) {
+      return one.run(blueprint);
+    };
+  })(),
+
+  /**
+   * Delete blueprint
+   * @param {Blueprint} blueprint
+   * @param {string} [key=id]
+   * @returns {SqliteResponse}
+   */
+  deleteBlueprint: (() => {
+    const oneId = _db.prepare("DELETE FROM blueprint WHERE id=?");
+    const oneName = _db.prepare("DELETE FROM blueprint WHERE name=?");
+    return function (blueprint, key) {
+      key ||= "id";
+      return key === "id" ? oneId.run(blueprint) : oneName.run(blueprint);
+    };
+  })(),
+
+  /**
+   * Create blueprintShape
+   * @param {BlueprintShape} blueprintShape
+   * @returns {SqliteResponse}
+   */
+  createBlueprintShape: (() => {
+    const one =
+      _db.prepare(`INSERT INTO blueprint_shape (blueprint_id, shape_id, count)
+VALUES (@id, @shape_id, @count)`);
+
+    return function (blueprintShape) {
+      return one.run(blueprintShape);
+    };
+  })(),
+
+  /**
+   * Get shapes
+   * @param {string[]} shapes
+   * @param {string} [key=id]
+   * @returns {Shape[]}
    */
   getShapes: (() => {
     const all = _db.prepare("SELECT * FROM shape");
@@ -69,46 +126,29 @@ const db = {
       return Array.isArray(shapes)
         ? some(shapes, key).all()
         : key === "id"
-          ? oneId.get(shapes)
-          : oneName.get(shapes);
-    };
-  })(),
-  updateShapes: (() => {
-    const oneId = _db.prepare(
-      "UPDATE shape SET name=@name, state=@state WHERE id=@id",
-    );
-    return function (shapes) {
-      return oneId.run(shapes);
+          ? oneId.all(shapes)
+          : oneName.all(shapes);
     };
   })(),
 
   /**
-   * @returns {Blueprint}
-   */
-  createBlueprints: (() => {
-    const one = _db.prepare(
-      "INSERT INTO blueprint (name, state) VALUES (@name, @state)",
-    );
-    return function (blueprints) {
-      return one.run(blueprints);
-    };
-  })(),
-
-  /**
-   * @returns {(Blueprint[]|Blueprint)}
+   * Get blueprints
+   * @param {string[]} blueprints
+   * @param {string} [key=id]
+   * @returns {Blueprint[]}
    */
   getBlueprints: (() => {
     const all = _db.prepare(`
 SELECT blueprint.id, blueprint.name, blueprint.state,
 shape.id AS shape_id, shape.name AS shape_name, shape.state AS shape_state,
-blueprint_shape.shape_count
+blueprint_shape.count
 FROM blueprint
 JOIN blueprint_shape ON blueprint.id=blueprint_shape.blueprint_id
 JOIN shape ON blueprint_shape.shape_id=shape.id`);
     const oneId = _db.prepare(`
 SELECT blueprint.id, blueprint.name, blueprint.state,
 shape.id AS shape_id, shape.name AS shape_name, shape.state AS shape_state,
-blueprint_shape.shape_count
+blueprint_shape.count
 FROM blueprint
 JOIN blueprint_shape ON blueprint.id=blueprint_shape.blueprint_id
 JOIN shape ON blueprint_shape.shape_id=shape.id
@@ -117,10 +157,10 @@ WHERE blueprint.id=?
     const oneName = _db.prepare(`
 SELECT blueprint.id, blueprint.name, blueprint.state,
 shape.id AS shape_id, shape.name AS shape_name, shape.state AS shape_state,
-blueprint_shape.shape_count
+blueprint_shape.count
 FROM blueprint
 JOIN blueprint_shape ON blueprint.id=blueprint_shape.blueprint_id
-JOIN shape on blueprint_shape.shape_id=shape.id
+JOIN shape ON blueprint_shape.shape_id=shape.id
 WHERE blueprint.name=?
 `);
     const some = (blueprints, key) => {
@@ -131,7 +171,7 @@ WHERE blueprint.name=?
       return _db.prepare(`
 SELECT blueprint.id, blueprint.name, blueprint.state,
 shape.id AS shape_id, shape.name AS shape_name, shape.state AS shape_state,
-blueprint_shape.shape_count
+blueprint_shape.count
 FROM blueprint
 JOIN blueprint_shape ON blueprint.id=blueprint_shape.blueprint_id
 JOIN shape ON blueprint_shape.shape_id=shape.id
@@ -160,10 +200,44 @@ WHERE blueprint.${key} IN (${parameters.slice(1)})
           id: blueprints_in[i].shape_id,
           name: blueprints_in[i].shape_name,
           state: blueprints_in[i].shape_state,
-          count: blueprints_in[i].shape_count,
+          count: blueprints_in[i].count,
         });
       }
       return Object.values(blueprints_out);
+    };
+  })(),
+
+  /**
+   * Get blueprint shapes
+   * @param {string[]} blueprintShapes
+   * @param {string} [key=blueprint_id]
+   * @returns {BlueprintShape[]}
+   */
+  getBlueprintShapes: (() => {
+    const all = _db.prepare("SELECT * from blueprint_shape");
+    const oneBlueprintId = _db.prepare(
+      "SELECT * FROM blueprint_shape WHERE blueprint_id=?",
+    );
+    const oneShapeId = _db.prepare(
+      "SELECT * FROM blueprint_shape WHERE shape_id=?",
+    );
+    const some = (blueprintShapes, key) => {
+      let parameters = "";
+      for (let i = 0; i < blueprintShapes.length; i++) {
+        parameters += `,'${blueprintShapes[i]}'`;
+      }
+      return _db.prepare(
+        `SELECT * FROM blueprint_shape WHERE ${key} IN (${parameters.slice(1)})`,
+      );
+    };
+    return function (blueprintShapes, key) {
+      if (!blueprintShapes) return all.all();
+      key ||= "blueprint_id";
+      return Array.isArray(blueprintShapes)
+        ? some(blueprintShapes, key).all()
+        : key === "blueprint_id"
+          ? oneBlueprintId.all(blueprintShapes)
+          : oneShapeId.all(blueprintShapes);
     };
   })(),
 };
