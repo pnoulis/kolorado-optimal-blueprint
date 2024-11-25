57bf3521415fbcf9620264fc31bec232e2854769
pavlos noulis <pavlos.noulis@gmail.com>
2024-11-25 23:27:56 +0200

    file restructuring toplevel

--- debug.js
+++
@@ -1,8 +0,0 @@
-export function debug(title) {
-  title && console.log(title);
-  title && console.log(">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>");
-  return (msg) => {
-    console.dir(msg, { depth: null });
-    title && console.log("<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<");
-  };
-}
